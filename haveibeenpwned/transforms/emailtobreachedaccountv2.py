5263e9543aea37fa7135647ef05f87916f2347d7
Christian Heinrich <christian.heinrich@cmlh.id.au>
2014-10-19 11:01:22 +1100

    Refactor PEP 8

--- haveibeenpwned/transforms/emailtobreachedaccountv2.py
+++ haveibeenpwned/transforms/emailtobreachedaccountv2.py
@@ -15,7 +15,7 @@ mt = MaltegoTransform()
 getrequrl = HIBP + email
 
 request = urllib2.Request(getrequrl)
-request.add_header('User-Agent','Maltego-haveibeenpwned')  # https://haveibeenpwned.com/API/v2#UserAgent 
+request.add_header('User-Agent', 'Maltego-haveibeenpwned')  # https://haveibeenpwned.com/API/v2#UserAgent 
 opener = urllib2.build_opener()
 
 try:
@@ -23,15 +23,15 @@ try:
     data = json.loads(response.read())
     response = data
     for breached in response:
-        mt.addEntity("maltego.Domain",breached['Domain'])
+        mt.addEntity("maltego.Domain", breached['Domain'])
 
 except urllib2.URLError, e:  # "Response Codes" within https://haveibeenpwned.com/API/v1
     
     if e.code == 400:
-        mt.addUIMessage("The e-mail account does not comply with an acceptable format",messageType="PartialError")
+        mt.addUIMessage("The e-mail account does not comply with an acceptable format", messageType="PartialError")
     
     if e.code == 404:
         UIMessage = email + " could not be found and has therefore not been pwned"
-        mt.addUIMessage(UIMessage,messageType="Inform")
+        mt.addUIMessage(UIMessage, messageType="Inform")
         
 mt.returnOutput()
